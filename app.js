fdd2ef9ae3c46056f05211d95ab3e0dc3ba83683
kiyaTolcha <tokiyatolcha@gmail.com>
2020-12-01 18:49:37 +0300

    initital commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us7.api.mailchimp.com/3.0/lists/049eccd5e7";
+  const option = {
+    method: "POST",
+    auth: "ECSPNewsLetter:586addc9e3e72ea5ac5c94d523f9a119-us7",
+  };
+  const request = https.request(url, option, function (response) {
+    const status = response.statusCode;
+    if (status === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", (data) => {
+      // console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+ request.end();
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening 3000");
+});
+
+//api keys
+//586addc9e3e72ea5ac5c94d523f9a119-us7
+//list id
+//049eccd5e7
+
+//mailchimp.com
+//username => ECSPNewsLetter
+//password => ECSP2:news
+
+//gmail.com
+//email => newsletter4ecsp@gmail.com
+//password => newsletter2ecsp
